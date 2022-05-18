d4566d18017db0b160db53f8566bdaafbe75b188
Artemxex <Artem.363636@gmail.com>
2022-05-18 19:11:08 +0300

    first1

--- src/Book.java
+++ src/Book.java
@@ -5,6 +5,11 @@ public class Book {
     private Author author;
     private int year;
 
+    public Book(String name, Author author, int year) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
 
     public String getName() {
         return this.name;
@@ -21,14 +26,6 @@ public class Book {
     public void setYear(int year) {
         this.year = year;
 
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return year == book.year && Objects.equals(name, book.name) && Objects.equals(author, book.author);
     }
 
     public String toString() {
