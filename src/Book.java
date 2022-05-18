4cb5c8c7ef6f90cf1ac2efad32c9a7e44f117f6e
Artemxex <Artem.363636@gmail.com>
2022-05-18 19:10:22 +0300

    first1

--- src/Book.java
+++ src/Book.java
@@ -5,11 +5,6 @@ public class Book {
     private Author author;
     private int year;
 
-    public Book(String name, Author author, int year) {
-        this.name = name;
-        this.author = author;
-        this.year = year;
-    }
 
     public String getName() {
         return this.name;
